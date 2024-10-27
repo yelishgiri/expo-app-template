b8e4de097b06237b4f46c0c238fb8e8f88dc5f48
yelish_giri1 <118233081+yelishgiri@users.noreply.github.com>
2024-10-27 01:02:24 -0500

    Push The Final Commit To Template Repo

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+        <Text className={"text-blue-200"}>Sample HomeScreen To Get You Statrted</Text>
+    </SafeAreaView>
+  )
+}
+
+
+export default Home;
